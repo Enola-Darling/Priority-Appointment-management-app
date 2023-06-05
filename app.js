50a3e8b9a763d6cd69a8ab1bab7f86d7b16536bd
enola-darling <luna.raimondo@gmail.com>
2023-06-05 19:13:54 +0200

    Initial app code

---
+++ app.js
@@ -0,0 +1,25 @@
+import { setGlobalArray } from "./utils/http.js";
+
+let listDate = []
+
+
+setGlobalArray('/todos')
+    .then(data => {
+        listDate = data.map((element) => {
+            element.priority = Math.floor(Math.random () * 6);
+            return element
+        });
+        console.log(listDate);    
+    }); 
+
+const getByPriority = (min, max) => {
+    const result = []
+    listDate.forEach((element) => {
+        if (min <= element.priority <= max) {
+            result.push(element)
+        }
+    })
+    return result
+}
+
+   
