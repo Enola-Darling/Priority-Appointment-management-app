0d75744f5d6d6cd76fc4c450acbdd7c6db25491c
enola-darling <luna.raimondo@gmail.com>
2023-06-06 18:08:57 +0200

    add new feature

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-import { setGlobalArray } from "./utils/http.js";
+import { setGlobalArray} from "./utils/http.js";
+import { getRandomDate} from "./utils/function.js";
 
 let listDate = []
-
 
 setGlobalArray('/todos')
     .then(data => {
         listDate = data.map((element) => {
             element.priority = Math.floor(Math.random() * 6);
+            element.date = getRandomDate(new Date('2023-01-01'), new Date('2023-12-31'))
             return element
         });
         console.log(listDate);    
--- utils/function.js
+++ utils/function.js
@@ -1,1 +1,34 @@
 
+export function getRandomDate(startDate, endDate) {
+    const timeDiff = endDate.getTime() - startDate.getTime();
+    const randomTime = Math.random() * timeDiff;
+    const randomDate = new Date(startDate.getTime() + randomTime);
+    return randomDate;
+}
+
+export const createEL = (type) => document.createElement(type);
+
+export const datesContent = (item, parent) => {
+    const dateItemEl = createEL("div");
+    const dateEl = createEL("div");
+    const dateTileEl = createEL("div");
+    const dateTimeEl = createEL("div");
+
+    dateItemEl.className = "dateItem"
+    dateEl.className = "date"
+    dateTileEl.className = "dateTile"
+    dateTimeEl.className = "dateTime"
+    dateEl.textContent = item.date.tiLocaleDateString()
+    dateTileEl.textContent = item.title
+    dateTimeEl.textContent = item.date.toLocaleTimeString()
+
+    dateItemEl.append(dateEl, dateTileEl, dateTimeEl);
+    parent.append(dateItemEl);
+
+}
+
+export const datesGen = (arr, parent) => {
+    arr.forEach(element => {
+        datesContent(element, parent)
+    });
+}
